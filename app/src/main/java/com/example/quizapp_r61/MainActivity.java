464ec4e042d69a8f26d35e50bd4c3418521b4ade
ACHBADFATIMA <fatiimaachbad@gmail.com>
2023-04-16 12:08:40 +0000

    SECOND_Commit_project_with_FIREBASE_ATHENTICATION_REGISTRATION

--- app/src/main/java/com/example/quizapp_r61/MainActivity.java
+++ app/src/main/java/com/example/quizapp_r61/MainActivity.java
@@ -1,23 +1,50 @@
 package com.example.quizapp_r61;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     //Step 1: Declaration
     EditText etLogin, etPassword;
     Button bLogin;
     TextView tvRegister;
+    FirebaseAuth mAuth;
+
+   /* @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent=new Intent(getApplicationContext(),Quiz1.class);
+            startActivity(intent);
+            finish();
+
+        }
+    }
+
+    */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
         //Step 2: Recuperation des ids
         etLogin = (EditText) findViewById(R.id.etMail);
         etPassword = (EditText) findViewById(R.id.etPassword);
@@ -27,13 +54,43 @@ public class MainActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String mail=etLogin.getText().toString();
+                String password=etPassword.getText().toString();
+                //String password1=etPassword1.getText().toString();
+                if(TextUtils.isEmpty(mail)){
+                    Toast.makeText(MainActivity.this,"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this,"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(mail, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Login Successful .",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent=new Intent(MainActivity.this,Quiz1.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
                 //Step 4: Traitement
-                if (etLogin.getText().toString().equals("toto") && etPassword.getText().toString().equals("123")){
+              /*  if (etLogin.getText().toString().equals("toto") && etPassword.getText().toString().equals("123")){
                     startActivity(new Intent(MainActivity.this, Quiz1.class));
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"Login or password incorrect !",Toast.LENGTH_SHORT).show();
-                }
+                }*/
             }
         });
         tvRegister.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/quizapp_r61/Register.java
+++ app/src/main/java/com/example/quizapp_r61/Register.java
@@ -7,52 +7,97 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Register extends AppCompatActivity {
-    EditText etMail, etPassword, etPassword1;
+    EditText etMail, etPassword;
     Button bRegister;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mUser = mAuth.getCurrentUser();
+        if(mUser != null){
+            Intent intent=new Intent(Register.this,MainActivity.class);
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         etMail=(EditText) findViewById(R.id.etMail);
         etPassword=(EditText) findViewById(R.id.etPassword);
-        etPassword1=(EditText)findViewById(R.id.etPassword1);
         bRegister=(Button)findViewById(R.id.bRegister);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String mail=etMail.getText().toString();
                 String password=etPassword.getText().toString();
-                String password1=etPassword1.getText().toString();
-                if(TextUtils.isEmpty(mail)){
-                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
-                    return;
+                //String password1=etPassword1.getText().toString();
+                if(mail.isEmpty()){
+                    Toast.makeText(Register.this,"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+
                 }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
-                    return;
+                else if(password.isEmpty()){
+                    Toast.makeText(Register.this,"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+
+                }else if(password.length()<6){
+                    Toast.makeText(Register.this,"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
+
+                }else {
+                    //progressDialog.setMessage("Please wait ....");
+                    mAuth.createUserWithEmailAndPassword(mail, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(),"Registration Successful!  inscription réussi! التسجيل ناجح! 註冊成功  ",Toast.LENGTH_LONG).show();
+
+                                        Intent intent=new Intent(Register.this,Quiz1.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        //startActivity(new Intent(Register.this,MainActivity.class));
+                                        //finish();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
                 }
-                if(TextUtils.isEmpty(password1)){
+
+
+
+            }
+               /* if(TextUtils.isEmpty(password1)){
                     Toast.makeText(getApplicationContext(),"Please confirm your password",Toast.LENGTH_SHORT).show();
                     return;
-                }
-                if(password.length()<6){
-                    Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(!password.equals(password1)){
+                }*/
+
+               /* if(!password.equals(password1)){
                     Toast.makeText(getApplicationContext(),"Please enter correct password",Toast.LENGTH_SHORT).show();
                     return;
-                }
+                }*/
 
                 //Commen.login=Mail;
                 //Commen.password=password;
-                Toast.makeText(getApplicationContext(),"Registration Successful!  inscription réussi! التسجيل ناجح! 註冊成功  ",Toast.LENGTH_LONG).show();
-                startActivity(new Intent(Register.this,MainActivity.class));
-                finish();
-            }
+
         });
     }
 }
